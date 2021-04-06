c1ed1878dbea1d15b3188af0a9790ba85fb85dfd
Carla Paiano <myemail@example.com>
2021-04-06 09:03:27 -0300

    Primeiro commit

---
+++ Backend/pizzaria/src/main/java/com/lead/pizzaria/entities/Client.java
@@ -0,0 +1,60 @@
+package com.lead.pizzaria.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String nome;
+    private String email;
+    private String tel;
+
+    public Client(long id, String nome, String email, String tel) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.tel = tel;
+    }
+
+    public Client() {
+
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+}
+
